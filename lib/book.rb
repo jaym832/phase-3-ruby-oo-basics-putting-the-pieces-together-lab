73860a4b65e8d9f1a97322ae2c40e19627f0082f
Javier Marin <jjmarin777@gmail.com>
2021-05-19 16:39:08 -0500

    commit tree ff4f4da1032253b597b9fcd5255cf4b76d94d1d1

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,54 @@
+class Book
+# require 'pry'
 
+    def initialize (title)
+
+        # binding.pry
+        @title=title 
+    end
+
+
+#getters
+    def title
+    @title
+    end
+
+
+
+    def author
+    @author
+    end
+
+
+
+    def genre
+    @genre
+    end
+
+    def page_count
+        @page_count
+    end
+
+#setters
+    def author= (author)
+    @author = author
+
+    end
+
+    def genre= (genre)
+    @genre = genre
+    end
+
+    def page_count=(page_count)
+        @page_count = page_count
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+
+
+
+end
 
